c14e790f4c135a6f7410b76e20e9fc019743997f
Nicksonmbembela1 <nicksonmbembela@gmail.com>
2026-07-13 21:53:04 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { supabase } from '../lib/supabaseClient'; // HAPA NDIO NIMEBADILI
+import { supabase } from '@/lib/supabaseClient'; // HAPA NDIO NIMEBADILI
 
 type ChatContact = { name: string; lastMessage: string; time: string }
 
