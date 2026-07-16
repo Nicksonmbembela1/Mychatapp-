cda10a503fcc379e1766d506d2d3f95cecd88728
Nicksonmbembela1 <nicksonmbembela@gmail.com>
2026-07-16 21:27:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,13 @@
-const express = require('express')
-const { createClient } = require('@supabase/supabase-js')
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const path = require('path');
 
-const app = express()
-app.use(express.json())
-app.use(cors())
-app.use(express.static('public')) 
+// Hii inasema: soma file zote za ndani ya folder ya "public"
+app.use(express.static('public')); 
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
-
-app.get('/api/messages', async (req, res) => {
-  const { data, error } = await supabase.from('messages').select('*').order('created_at')
-  if(error) return res.status(500).json({error})
-  res.json(data)
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
-app.post('/api/messages', async (req, res) => {
-  const { text } = req.body
-  const { error } = await supabase.from('messages').insert([{ text }])
-  if(error) return res.status(500).json({error})
-  res.json({ success: true })
-})
-
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`Mychatapp iko live kwenye ${PORT}`))
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Mychatapp ina-run kwenye port ${PORT}`));
